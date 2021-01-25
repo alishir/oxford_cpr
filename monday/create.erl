3074caa6148fa21adff66df233a8f56a53c8a2b6
Nicholas Drake <hellolao8n@gmail.com>
2021-01-25 22:10:15 +0000

    added working implementation of db.erl

--- monday/create.erl
+++ monday/create.erl
@@ -16,9 +16,15 @@ create_base_case_0(N, Buff) ->
 create_base_case_n(N) -> 
   create_base_case_n(1, N).
 create_base_case_n(N, N) -> [N];
-create_base_case_n(M, N) -> 
+create_base_case_n(M, N) -> % not tail recursive
   [M|create_base_case_n(M+1, N)].
 
+% cannot make above tail recursive
+% create_base_case_n_tail(N) ->
+%   create_base_case_n_tail(1, N, []).
+% create_base_case_n_tail(N, N, Buff) -> [N | Buff];
+% create_base_case_n_tail(M, N, Buff) ->  
+%   create_base_case_n_tail(M + 1, N, [M | Buff]).
 reverse_create(1) -> [1];
 reverse_create(N) -> [N | reverse_create(N-1)]. 
 %% reverse_create(N) -> [N] ++ reverse_create(N-1).
--- monday/db.erl
+++ monday/db.erl
@@ -1,15 +1,44 @@
 -module(db).
-% -export([new/0, destroy/1, write/3, delete/2, read/2, match/2]).
+-export([new/0, destroy/1, write/3, delete/2, read/2, match/2]).
 
+% new() -> DbRef
 new() -> [].
 
-% destroy(DbRef) ->
+% destroy(DbRef) -> ok
+destroy(_) -> ok.
 
+% write(Key, Element, DbRef) -> NewDbRef
 write(Key, Element, DbRef) -> 
-  [{Key, Element} | DbRef].
-% delete(Key, DbRef) ->
+  DbRefDeleted = delete(Key, DbRef),
+  [{Key, Element} | DbRefDeleted].
 
-% read(Key, DbRef) ->
 
-% match(Element, DbRef) ->
+% read(Key, DbRef) -> {ok, Element} | {error, instance}
+read(Key, [{Key, Element} | _]) -> 
+  {ok, Element};
+read(Key, [_ | DbRefTail]) -> 
+  read(Key, DbRefTail);
+read(_, []) -> 
+  {error, instance}.
 
+% match(Element, DbRef) -> [Key1, ..., KeyN]
+match(Element, DbRef) ->
+  match_acc(Element, DbRef, []).
+
+match_acc(Element, [{Key, Element} | DbRefTail], Matched) ->
+  match_acc(Element, DbRefTail, [Key | Matched]);
+match_acc(Element, [_ | DbRefTail], Matched) ->
+  match_acc(Element, DbRefTail, Matched);
+match_acc(_, [], Matched) ->
+  Matched.
+
+% delete(Key, DbRef) -> NewDbRef
+delete(Key, DbRef) ->
+  delete_acc(Key, DbRef, []).
+
+delete_acc(Key, [{Key, _} | DbRefTail], DbRefPassed) ->
+  delete_acc(Key, DbRefTail, DbRefPassed);
+delete_acc(Key, [KeyElement | DbRefTail], DbRefPassed) ->
+  delete_acc(Key, DbRefTail, [KeyElement | DbRefPassed]);
+delete_acc(_, [], DbRefPassed) ->
+  DbRefPassed.
